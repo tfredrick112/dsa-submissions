ead44ffb1978aa58fdbfdb4d9082132f1d93239d
tfredrick112 <38160688+tfredrick112@users.noreply.github.com>
2026-05-12 19:01:20 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+
+        // dp[i][j] is the maximum profit that you get starting on day with index i and
+        // with a starting state of j
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int buy = 0; buy <= 1; buy++)
+            {
+                if (buy == 1)
+                {
+                    dp[i][buy] = max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
+                }
+                else
+                {
+                    dp[i][buy] = max(prices[i] + dp[i + 2][1], 0 + dp[i + 1][0]);
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+};
