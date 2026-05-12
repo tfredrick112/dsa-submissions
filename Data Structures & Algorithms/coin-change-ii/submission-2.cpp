39531bd3340146f3293001ce040d18eb5bb39c42
tfredrick112 <38160688+tfredrick112@users.noreply.github.com>
2026-05-12 12:36:30 +0530

    Update submission-2.cpp

--- Data Structures & Algorithms/coin-change-ii/submission-2.cpp
+++ Data Structures & Algorithms/coin-change-ii/submission-2.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+    // numWays(coins, amount, idx, dp) -> number of ways to get a sum equal to amount by using entries from the coins array, from index idx onwards
     int numWays(vector<int>& coins, int amount, int idx, vector<vector<int>>& dp)
     {
         if (amount == 0)
         {
+            // amount has become 0, so we have found a valid way to get a sum equal to the original input
             return 1;
         }
         else if (amount < 0)
@@ -15,8 +17,13 @@ public:
             return dp[idx][amount];
         
         int total = 0;
+
+        // The loop starts at idx, because we do not want to create duplicate solutions. For example, if we are trying to get a sum of 4 using [1,2,3]
+        // [1,3] and [3,1] will add up to 4, but we have to avoid such duplicates, so we ensure that we only look ahead, so 1 + 3 will be found, but if
+        // 3 is the first number, then it will not use 1 as it is on the left.
         for (int i = idx; i < coins.size(); i++)
         {
+            // For idx param, we send i and NOT i + 1, because we have an unlimited supply of coins of each denomination.
             total += numWays(coins, amount - coins[i], i, dp);
         }
 
